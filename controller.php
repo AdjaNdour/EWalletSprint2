aa7d9d02db02df58683ca5178d958f0a32616326
AdjaNdour <adjacourandour4@gmail.com>
2026-06-29 22:15:07 +0000

    ajout du menu interactif

--- controller.php
+++ controller.php
@@ -1,3 +1,26 @@
 <?php
 
+function controller($choix){
+
+    switch($choix){
+
+        case 1:
+            echo "1";
+            break;
+        case 2:
+            echo "2"; 
+            break;
+        case 3:
+            echo "3";
+            break;
+        case 4:
+            echo "4";
+            break;
+        default:
+            echo "Au revoir \n";
+            break;
+    }
+
+}
+
 ?>
